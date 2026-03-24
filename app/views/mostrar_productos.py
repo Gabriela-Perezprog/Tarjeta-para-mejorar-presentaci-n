79200d41253a29ec3909c938b10179e3bb8bc42b
gabrielaperezweb <gabrielaperezs21122006@gmail.com>
2026-03-23 19:18:12 -0600

    Actualización completa

--- app/views/mostrar_productos.py
+++ app/views/mostrar_productos.py
@@ -8,45 +8,51 @@ from app.styles.estilos import Colors, Textos, Card
 def products_view(page:ft.Page) -> ft.Control: 
     rows_data:list[dict[str,Any]]=[]
     total_items=0 
+
     total_text = ft.Text("Total de productos: (cargando...)", style=Textos.H3) 
-    #Encabezados 
-    columnas=[ ft.DataColumn(label=ft.Text("Nombre", style=Textos.H3)), 
-        ft.DataColumn(label=ft.Text("Cantidad", style=Textos.H3)), 
-        ft.DataColumn(label=ft.Text("Ingreso", style=Textos.H3)), 
-        ft.DataColumn(label=ft.Text("Min", style=Textos.H3)), 
-        ft.DataColumn(label=ft.Text("Max", style=Textos.H3)),
+
+    # Encabezados 
+    columnas=[ 
+        ft.DataColumn(label=ft.Text("Nombre", style=Textos.H3, color="#1e293b")), 
+        ft.DataColumn(label=ft.Text("Cantidad", style=Textos.H3, color="#1e293b")), 
+        ft.DataColumn(label=ft.Text("Ingreso", style=Textos.H3, color="#1e293b")), 
+        ft.DataColumn(label=ft.Text("Min", style=Textos.H3, color="#1e293b")), 
+        ft.DataColumn(label=ft.Text("Max", style=Textos.H3, color="#1e293b")),
     ]
 
-    #Se definen las filas de la tabla 
-    # Cada data.append agrega 
+    # Datos de prueba 
     data=[] 
     data.append( 
         ft.DataRow( 
             cells=[ 
-                ft.DataCell(ft.Text("nombre1...")), 
-                ft.DataCell(ft.Text("cantidad1...")),
-                ft.DataCell(ft.Text("ingreso1...")), 
-                ft.DataCell(ft.Text("min1...")), 
-                ft.DataCell(ft.Text("max1...")), ]))
+                ft.DataCell(ft.Text("nombre1...", color="#334155")), 
+                ft.DataCell(ft.Text("cantidad1...", color="#334155")),
+                ft.DataCell(ft.Text("ingreso1...", color="#334155")), 
+                ft.DataCell(ft.Text("min1...", color="#334155")), 
+                ft.DataCell(ft.Text("max1...", color="#334155")),
+            ],
+            color="#f8fafc"
+        )
+    )
 
-    #Se crea la tabla con los encabezados(columnas) y los datos de prueba(data) 
+    # Tabla
     tabla=ft.DataTable( 
         columns=columnas, 
         rows=data, 
         width=900, 
         heading_row_height=60, 
-        heading_row_color=Colors.BG, 
+        heading_row_color="#e2e8f0", 
         data_row_max_height=60, 
-        data_row_min_height=48
-        )
-    #return tabla
+        data_row_min_height=48,
+        border=ft.border.all(1, "#cbd5f5"),
+        column_spacing=40
+    )
 
     async def actualizar_data():
         nonlocal rows_data, total_items
         try:
-            data=list_products(limit=500, offset=0) # se conecta a transaccines_api_productos para obtener los product
+            data=list_products(limit=500, offset=0)
             total_items=int(data.get("total", 0)) 
-            #print(total_items)
             total_text.value="Total de productos: "+str(total_items)
             rows_data=data.get("items", []) or []
             actualizar_filas()
@@ -59,13 +65,39 @@ def products_view(page:ft.Page) -> ft.Control:
             nuevas_filas.append( 
                 ft.DataRow( 
                     cells=[ 
-                        ft.DataCell(ft.Text(p.get("name", ""))), 
-                        ft.DataCell(ft.Text(str(p.get("quantity", "")))),
-                        ft.DataCell(ft.Text(p.get("ingreso_date", ""))), 
-                        ft.DataCell(ft.Text(str(p.get("min_stock", "")))), 
-                        ft.DataCell(ft.Text(str(p.get("max", "")))), ])) 
+                        ft.DataCell(ft.Text(p.get("name", ""), color="#334155")), 
+                        ft.DataCell(ft.Text(str(p.get("quantity", "")), color="#334155")),
+                        ft.DataCell(ft.Text(p.get("ingreso_date", ""), color="#334155")), 
+                        ft.DataCell(ft.Text(str(p.get("min_stock", "")), color="#334155")), 
+                        ft.DataCell(ft.Text(str(p.get("max", "")), color="#334155")),
+                    ],
+                    color="#f8fafc"
+                )
+            ) 
         tabla.rows=nuevas_filas
         page.update()
-    page.run_task(actualizar_data)
-    return tabla
 
+    page.run_task(actualizar_data)
+
+    # SE CREA LA TARJETA
+    contenido = ft.Column([
+        total_text,
+        tabla
+    ])
+
+    tarjeta = ft.Container(
+        content=contenido,
+        **Card.tarjeta
+    )
+
+    
+    # return tarjeta
+
+    #CONTENEDOR PARA CENTRAR
+    final = ft.Container(
+        expand=True,
+        alignment=ft.Alignment(0, -1),
+        content=tarjeta
+    )
+
+    return final
